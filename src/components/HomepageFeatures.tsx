860dd4d574cedbdb1b89e6d52a93e0552bd8fe2d
Daniel Karnutsch <daniel.karnutsch@vivid-planet.com>
2022-04-13 11:31:34 +0200

    add prettier

--- src/components/HomepageFeatures.tsx
+++ src/components/HomepageFeatures.tsx
@@ -4,70 +4,58 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
-import React from 'react';
-import clsx from 'clsx';
-import styles from './HomepageFeatures.module.css';
+import React from "react";
+import clsx from "clsx";
+import styles from "./HomepageFeatures.module.css";
 
 type FeatureItem = {
-  title: string;
-  image: string;
-  description: JSX.Element;
+    title: string;
+    image: string;
+    description: JSX.Element;
 };
 
 const FeatureList: FeatureItem[] = [
-  {
-    title: 'Easy to Use',
-    image: '/img/feature.png',
-    description: (
-      <>
-            Lorem Ipsum Feature A
-      </>
-    ),
-  },
-  {
-    title: 'Focus on What Matters',
-      image: '/img/feature.png',
-    description: (
-      <>
-          Lorem Ipsum Feature B
-      </>
-    ),
-  },
-  {
-    title: 'Powered by React',
-      image: '/img/feature.png',
-    description: (
-      <>
-          Lorem Ipsum Feature C
-      </>
-    ),
-  },
+    {
+        title: "Easy to Use",
+        image: "/img/feature.png",
+        description: <>Lorem Ipsum Feature A</>,
+    },
+    {
+        title: "Focus on What Matters",
+        image: "/img/feature.png",
+        description: <>Lorem Ipsum Feature B</>,
+    },
+    {
+        title: "Powered by React",
+        image: "/img/feature.png",
+        description: <>Lorem Ipsum Feature C</>,
+    },
 ];
 
-function Feature({title, image, description}: FeatureItem) {
-  return (
-    <div className={clsx('col col--4')}>
-      <div className="text--center">
-        <img className={styles.featureSvg} alt={title} src={image} />
-      </div>
-      <div className="text--center padding-horiz--md">
-        <h3>{title}</h3>
-        <p>{description}</p>
-      </div>
-    </div>
-  );
+function Feature({ title, image, description }: FeatureItem) {
+    return (
+        <div className={clsx("col col--4")}>
+            <div className="text--center">
+                <img className={styles.featureSvg} alt={title} src={image} />
+            </div>
+            <div className="text--center padding-horiz--md">
+                <h3>{title}</h3>
+                <p>{description}</p>
+            </div>
+        </div>
+    );
 }
 
 export default function HomepageFeatures(): JSX.Element {
-  return (
-    <section className={styles.features}>
-      <div className="container">
-        <div className="row">
-          {FeatureList.map((props, idx) => (
-            <Feature key={idx} {...props} />
-          ))}
-        </div>
-      </div>
-    </section>
-  );
+    return (
+        <section className={styles.features}>
+            <div className="container">
+                <div className="row">
+                    {FeatureList.map((props, idx) => (
+                        <Feature key={idx} {...props} />
+                    ))}
+                </div>
+            </div>
+        </section>
+    );
 }
